d8cb9b74ea8ce4e32c840c0b439ba4ebf5d421e6
Rajesh Kumar Yadav <115943147+Rajesh23965@users.noreply.github.com>
2025-07-24 18:03:48 +0545

    Initial Commit

---
+++ SubSequences.java
@@ -0,0 +1,20 @@
+public class SubSequences {
+    public static void subSequences(String str, int idx, String newString) {
+
+        if (idx == str.length()) {
+            System.err.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        // to be
+        subSequences(str, idx + 1, newString + currChar); // include current character
+
+        // not to be
+        subSequences(str, idx+1, newString);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        subSequences(str, 0, "");
+    }
+}
