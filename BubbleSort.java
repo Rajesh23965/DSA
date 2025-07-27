b030354df430b077c07f7e7fc2547ed8bf4da456
Rajesh Kumar Yadav <115943147+Rajesh23965@users.noreply.github.com>
2025-07-27 18:02:58 +0545

    Initial commit

---
+++ BubbleSort.java
@@ -0,0 +1,33 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) { 
+            for (int j = 0; j < n - i - 1; j++) { 
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {5, 1, 4, 2, 8};
+        System.out.println("Array before sorting:");
+        for (int i : data) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        bubbleSort(data);
+
+        System.out.println("Array after sorting:");
+        for (int i : data) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
